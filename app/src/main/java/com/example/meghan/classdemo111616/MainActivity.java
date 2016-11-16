8c36a1d4ef9b0af43e07aa849c8ab359d1a3de16
Meghan <megshee@umich.edu>
2016-11-16 09:19:19 -0500

    test change

--- app/src/main/java/com/example/meghan/classdemo111616/MainActivity.java
+++ app/src/main/java/com/example/meghan/classdemo111616/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    //Change here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
